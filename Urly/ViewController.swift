c63afc03c6e311a48de89647b267e56c115842a5
Taavi <taavi@taavis-macbook-pro.local>
2019-03-26 09:34:20 +0200

    Initial Commit

---
+++ Urly/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Urly
+//
+//  Created by Taavi Roosimägi on 26/03/2019.
+//  Copyright © 2019 Taavi Roosimägi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
